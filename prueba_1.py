e1f60714fbc2939dfadf72f0ab85c39582a81fc0
ignacio <ig.sariego@duocuc.cl>
2025-06-23 22:53:16 -0400

    Commit de la parte 2. Emitido por Ignacio Sariego.

--- prueba_1.py
+++ prueba_1.py
@@ -2,13 +2,23 @@ def menu():
     lista_alumnos = []
     while True:
         try:
-            eleccion = int(input("\n1) Agregar alumnos a la lista. \n2) Mostrar todos los alumnos. \n3) Salir.\nElige una opcion: "))
+            eleccion = int(input("\n1) Agregar alumnos a la lista. \n2) Mostrar todos los alumnos.\n3) Eliminar alumno. \n4) Salir.\nElige una opcion: "))
             if eleccion == 1:
                 alumno = input("Ingrese el nombre del alumno: ").lower()
                 lista_alumnos.append(alumno)
             elif eleccion == 2:
                 print (f"Los nombres en la lista son: {lista_alumnos}")
-            elif eleccion == 3:
+            elif eleccion ==3:
+                alumno_eliminado = input("Escribe el nombre del alumno a eliminar: ").lower()
+                if alumno_eliminado.isalpha():
+                    for i in lista_alumnos:
+                        if i == alumno_eliminado:
+                            lista_alumnos.remove(alumno_eliminado)
+                        else:
+                            print(f"No hay un alumno llamado {alumno_eliminado} en la lista.")
+                else:
+                    print("El nombre solo puede estar en letras.")
+            elif eleccion == 4:
                 print ("Adiós.")
                 break
             else:
